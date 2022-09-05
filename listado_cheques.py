82f0226574d45d66dd082b94c0458770dbb78742
Esdreelxs <105323148+Esdreelxs@users.noreply.github.com>
2022-09-05 16:05:33 -0300

    Agrego filtrado por estado

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,4 +1,15 @@
-#python listado_cheques.py cuentas.csv 1617591371 CVS EMITIDO APROBADO 12-05-2000:25-05-2000
+"""
+CVS
+python listado_cheques.py cuentas.csv 1617591371 CVS EMITIDO APROBADO 12-05-2000:25-05-2000
+
+PANTALLA
+python listado_cheques.py cuentas.csv 23665789 pantalla EMITIDO APROBADO 12-05-2000:25-05-2000
+
+REPETIDO
+python listado_cheques.py cuentas.csv 23665789 PANTALLA EMITIDO APROBADO 12-05-2000:25-05-2000
+
+"""
+23665789
 import sys
 import csv
 from datetime import datetime
@@ -7,6 +18,14 @@ from datetime import datetime
 nomb_csv=sys.argv[1]
 dni_cliente=sys.argv[2]
 salida=sys.argv[3]
+stdo_cheque=sys.argv[5]
+rango_fecha=sys.argv[6]
+
+#Filtrado de cheques
+#filtrados por el estado depositado o emitido
+cheque_filtrado = []
+# Los cheques filtrados por el estado 
+cheque_estado = []  
 
 
 entrada=""
@@ -27,11 +46,12 @@ with open(nomb_csv) as File:
         listaUsuario.append(row)
 
 #Filtra el dni ingresado
-usuario=(list(filter(lambda item: item['DNI'] == dni_cliente, listaUsuario)))
+usuario=(list(filter(lambda item: item['DNI'] == dni_cliente , listaUsuario)))
+#Filtra el estado
+cheque_filtrado=(list(filter(lambda item: item['Estado'] == stdo_cheque , usuario )))
 
 #fecha y numero de cheque
 for i in usuario:
-    datos= usuario
     origen=int(i['FechaOrigen'])
     pago=int(i['FechaPago'])
     origen= str(datetime.fromtimestamp(origen))
@@ -56,32 +76,23 @@ if cheques[maximo]!=1:
 if cheques[maximo]==0:
     print("ERROR: No existen cheques")
     
-
-
 #Salidas    
-if salida== "CSV" or salida== "csv":
+if salida== "CVS" or salida== "cvs":
     archivo = open(f"{dni_cliente}_AT_{datetime.today().strftime('%Y-%m-%d')}.csv", "w", newline='')
     spamreader = csv.writer(archivo)
     spamreader.writerow(archivoCSV)
     archivo.close()   
 
+
 if salida== "PANTALLA" or salida== "pantalla":
-    for i in usuario:
-        print("Usuario: "+i['DNI']+" Nro de cheque: "+i['NroCheque']+" Tipo de cheque: "+i['Tipo']+" Estado del cheque: "+i['Estado'])
-      
-#Filtrado de cheques
-#filtrados por el estado depositado o emitido
-cheque_filtrado = []
-# Los cheques filtrados por el estado 
-cheque_estado = []  
-
-stdo_cheque=sys.argv[4]
-rango_fecha=sys.argv[6]
-
-
-
-print(usuario)
-print(stdo_cheque,cheque_filtrado)
-
+    #Agregue esta condicion por que si el usuario no agrega un parametro de estado, no se podra realizar el filtrado haciendo que la lista
+    #no contenga ningun dato, entoces se utiliza la lista usuario ya que permite que aparezcan todos los datos relacionados con lacuenta del usuario solicitado
+    if len(cheque_filtrado) == 0: 
+        for i in usuario:
+            print("Usuario: "+i['DNI']+" Nro de cheque: "+i['NroCheque']+" Tipo de cheque: "+i['Tipo']+" Estado del cheque: "+i['Estado'])
+    else:  
+        for a in cheque_filtrado:
+            print("Usuario: "+a['DNI']+" Nro de cheque: "+a['NroCheque']+" Tipo de cheque: "+a['Tipo']+" Estado del cheque: "+a['Estado'])
+  
 
 
